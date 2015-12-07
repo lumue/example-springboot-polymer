245e256cd2bdcb14de4b09f809e60c0a95946e05
lm <mueller.lutz@gmail.com>
2015-12-07 19:18:34 +0100

    initial commit

--- springboot-server/src/main/java/hello/GreetingController.java
+++ springboot-server/src/main/java/hello/GreetingController.java
@@ -11,20 +11,15 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @Controller
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "%s";
     private final AtomicLong counter = new AtomicLong();
 
-    @CrossOrigin(origins = "http://localhost:9000")
+    @CrossOrigin(origins = "*")
     @RequestMapping("/greeting")
-    public @ResponseBody Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
+    public @ResponseBody Greeting greeting(@RequestParam(required=false, defaultValue="Echoooooo") String name) {
         System.out.println("==== in greeting ====");
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
-    @RequestMapping("/greeting-javaconfig")
-    public @ResponseBody Greeting greetingWithJavaconfig(@RequestParam(required=false, defaultValue="World") String name) {
-        System.out.println("==== in greeting ====");
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
 
 }
